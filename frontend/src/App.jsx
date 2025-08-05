f55ec611fbb5a1196680ba38c00c6fca4024e1a6
Tingan <syeak123@gmail.com>
2025-08-05 13:45:29 +0800

    feat: create basic MoodTracker component with static UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MoodTracker from './components/MoodTracker'
+import './index.css';
+function App() {
+  return <MoodTracker />
+}
+
+export default App
---
+++ frontend/src/components/MoodTracker.jsx
@@ -0,0 +1,16 @@
+
+const MoodTracker=()=>{
+    return(
+        <div className="min-h-screen bg-gray-100 py-8">
+            <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
+                <h1 className="">
+                    今日心情紀錄
+                </h1>
+                <div className="">
+                    <p className="">今天還沒紀錄心情</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+export default MoodTracker;
